3f3d76ad30f958d84ac7526740df1060d6bb4826
Roger Silva <rogersilvati@gmail.com>
2025-11-03 16:46:07 -0300

    criado o primeiro app

---
+++ meu_app_1/bin/meu_app_1.dart
@@ -0,0 +1,5 @@
+import 'package:meu_app_1/meu_app_1.dart' as meu_app_1;
+
+void main(List<String> arguments) {
+  print('Hello world: ${meu_app_1.calculate()}!');
+}
---
+++ meu_app_1/test/meu_app_1_test.dart
@@ -0,0 +1,8 @@
+import 'package:meu_app_1/meu_app_1.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
