9cbb9fe674109c3d906c0b889b4dcda28a99128a
Roger Silva <rogersilvati@gmail.com>
2025-11-16 14:39:34 -0300

    Finish calculator

--- calculadora/bin/calculadora.dart
+++ calculadora/bin/calculadora.dart
@@ -1,8 +1,69 @@
 import 'dart:io';
 
 void main() {
-  double numeroUm = double.parse(stdin.readLineSync()!);
-  double numeroDois = double.parse(stdin.readLineSync()!);
+  double numeroUm = 0;
+  double numeroDois = 0;
+  String operacao = "";
+  String entrada;
+  List<String> operacoesValidas = ['+', '-', '*', '/'];
 
-  print(numeroUm + numeroDois);
+  void soma() {
+    print(numeroUm + numeroDois);
+  }
+
+  void subtracao() {
+    print(numeroUm - numeroDois);
+  }
+
+  void multiplicacao() {
+    print(numeroUm * numeroDois);
+  }
+
+  void divisao() {
+    print(numeroUm / numeroDois);
+  }
+
+  void calcular() {
+    if (operacao == '+') {
+      soma();
+    } else if (operacao == '-') {
+      subtracao();
+    } else if (operacao == '*') {
+      multiplicacao();
+    } else if (operacao == '/') {
+      divisao();
+    } else {
+      print('Operação inválida');
+    }
+  }
+
+  void getOperacao() {
+    print('Informe a operação ${operacoesValidas.toString()}:');
+    entrada = stdin.readLineSync()!;
+    if (entrada != null &&
+        entrada.isNotEmpty &&
+        operacoesValidas.contains(entrada)) {
+      operacao = entrada;
+    } else {
+      print('Operação inválida');
+      getOperacao();
+    }
+  }
+
+  print('Informe o primeiro número:');
+  entrada = stdin.readLineSync()!;
+  if (entrada != null && entrada.isNotEmpty) {
+    numeroUm = double.parse(entrada);
+  }
+
+  getOperacao();
+
+  print('Informe o segundo número:');
+  entrada = stdin.readLineSync()!;
+  if (entrada != null && entrada.isNotEmpty) {
+    numeroDois = double.parse(entrada);
+  }
+
+  print("O resultado é: ");
+  calcular();
 }
