fdedf5281fce626dcbc5431eaaf2eac7344b6a80
Roger Silva <rogersilvati@gmail.com>
2025-11-16 13:05:39 -0300

    Criado projeto calculadora

---
+++ calculadora/bin/calculadora.dart
@@ -0,0 +1,8 @@
+import 'dart:io';
+
+void main() {
+  double numeroUm = double.parse(stdin.readLineSync()!);
+  double numeroDois = double.parse(stdin.readLineSync()!);
+
+  print(numeroUm + numeroDois);
+}
